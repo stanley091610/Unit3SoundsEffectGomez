c20e287ff40b4f4d3dfaf98e82e347aeb7ab7da9
stanley091610 <754083@student.dusd.net>
2025-05-06 10:10:52 -0700

    add new scripts and almost finish

--- P2SoundandEffects/Assets/Course Library/scripts/PlayerController.cs
+++ P2SoundandEffects/Assets/Course Library/scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -13,8 +14,11 @@ public class PlayerController : MonoBehaviour
     public AudioClip crashSound;
     public float jumpForce = 10;
     public float gravityModifier;
+    public float doubleJumpForce;
     public bool isOnGround = true;
     public bool gameOver = false;
+    public bool doubleJumpUsed = false;
+    public bool doubleSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +32,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Muultiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
+        }
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -35,7 +49,14 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            doubleJumpUsed = false;
         }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        doubleJumpUsed = true;
+        playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+        playerAnim.Play("Running_Jump", 3, 0f);
+        playerAudio.PlayOneShot(jumpSound, 1.0f);
     }
    
     private void OnCollisionEnter(Collision collision)
