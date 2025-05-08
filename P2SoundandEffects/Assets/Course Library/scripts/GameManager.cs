e97e40430d49d6fa367b464ea24520aec27a1eb2
stanley091610 <754083@student.dusd.net>
2025-05-08 09:43:40 -0700

    I finish the scripting GameManager

--- P2SoundandEffects/Assets/Course Library/scripts/GameManager.cs
+++ P2SoundandEffects/Assets/Course Library/scripts/GameManager.cs
@@ -2,35 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLeft : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    private float speed = 30;
+    public float score;
     private PlayerController playerControllerScript;
-    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerControllerScript.gameOver == false)
+        if(!playerControllerScript.gameOver)
         {
             if (playerControllerScript.doubleSpeed)
             {
-                transform.Translate(Vector3.left * Time.deltaTime * (speed * 2));
+                score += 2;
             }
             else
             {
-                transform.Translate(Vector3.left * Time.deltaTime * speed);
+                score++;
             }
-        } 
-        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
-        {
-            Destroy(gameObject);
+            Debug.Log("Score: " + score);
         }
     }
 }
